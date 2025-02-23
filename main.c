d51d962bc9f616e3982ea045ed3a1a38dad3eb32
Emre <emresert2233@gmail.com>
2025-02-23 17:43:06 +0300

    Update main.c
    fonksiyonlar head üzerinde değişiklik yapamıyordu,head'in adresi fonksiyonlara geçirildi, böylece liste başındaki değişiklikler doğru şekilde yapılabiliyor

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-//Program boyunca kullanilan "iter" pointer degiskeni mevcut dugumu isaret etmek icin kullanilmistir.
+// Program boyunca kullan�lan "iter" pointer de�i�keni mevcut d���m� i�aret etmek i�in kullan�lm��t�r.
 
 struct node
 {
@@ -13,66 +13,70 @@ struct node
     int sinif;
     struct node* next;
     struct node* prev;
+};
 
-}* head;
-
-//ogrenci bilgileri aliniyor.
-void ogrEkle()
+// ogrenci bilgileri aliniyor
+void ogrEkle(struct node** head)
 {
     struct node ogr;
     printf("\nOgrencinin numarasini giriniz: ");
-    scanf("%d",&(ogr.no));
+    scanf("%d", &(ogr.no));
     printf("Ogrencinin adini giriniz: ");
-    scanf("%s",&(ogr.ad));
+    scanf("%s", &(ogr.ad));
     printf("Ogrencinin soyadini giriniz: ");
-    scanf("%s",&(ogr.soyad));
+    scanf("%s", &(ogr.soyad));
     printf("Ogrencinin bolumunu giriniz: ");
-    scanf("%s",&(ogr.bolum));
+    scanf("%s", &(ogr.bolum));
     printf("Ogrencinin sinifini giriniz: ");
-    scanf("%d",&(ogr.sinif));
+    scanf("%d", &(ogr.sinif));
     printf("Ogrenci eklendi!\n");
 
-    dugumeOgrEkle(ogr.no,ogr.ad,ogr.soyad,ogr.bolum,ogr.sinif);
+    dugumeOgrEkle(head, ogr.no, ogr.ad, ogr.soyad, ogr.bolum, ogr.sinif);
 }
 
-//ogrEkle fonksiyonu ile alinan parametreler dugume eklenmek icin dugumeOgrEkle fonksiyonuna aktariliyor.
-void dugumeOgrEkle(int ogr_no,char ogr_ad[],char ogr_soyad[],char ogr_bolum[],int ogr_sinif)
+// ogrEkle fonksiyonu ile al�nan parametreler d���me eklenmek i�in dugumeOgrEkle fonksiyonuna aktar�l�yor
+void dugumeOgrEkle(struct node** head, int ogr_no, char ogr_ad[], char ogr_soyad[], char ogr_bolum[], int ogr_sinif)
 {
-    struct node * iter = (struct node*)malloc(sizeof(struct node));
+    struct node* iter = (struct node*)malloc(sizeof(struct node));
+    if (iter == NULL)
+    {
+        printf("Hafiza tahsisi basarisiz oldu!\n");
+        exit(1); // Bellek tahsisi ba�ar�s�zsa program� sonland�r�yoruz
+    }
     iter->no = ogr_no;
-    strcpy(iter->ad,ogr_ad);
-    strcpy(iter->soyad,ogr_soyad);
-    strcpy(iter->bolum,ogr_bolum);
+    strcpy(iter->ad, ogr_ad);
+    strcpy(iter->soyad, ogr_soyad);
+    strcpy(iter->bolum, ogr_bolum);
     iter->sinif = ogr_sinif;
     iter->next = NULL;
 
-    if(head==NULL)
+    if (*head == NULL)
     {
-        head=iter;
+        *head = iter;
     }
     else
     {
-        iter->next = head;
-        head = iter;
+        iter->next = *head;
+        *head = iter;
     }
 }
 
-//numarasi alinan ogrencinin silme islemi yapiliyor.
-void ogrSil()
+// numaras� al�nan ��rencinin silme i�lemi yap�l�yor
+void ogrSil(struct node** head)
 {
-    struct node * iter1 = head;
-    struct node * iter2 = head;
+    struct node* iter1 = *head;
+    struct node* iter2 = *head;
     int num;
     printf("Silmek istediginiz ogrencinin numarasini giriniz: ");
-    scanf("%d",&num);
+    scanf("%d", &num);
 
-    while(iter1!=NULL)
+    while (iter1 != NULL)
     {
-        if(iter1->no == num)
+        if (iter1->no == num)
         {
-            if(iter1==iter2)
+            if (iter1 == iter2)
             {
-                head = head->next;
+                *head = (*head)->next;
                 free(iter1);
             }
             else
@@ -86,75 +90,77 @@ void ogrSil()
         iter2 = iter1;
         iter1 = iter1->next;
     }
-    printf("%d numarali ogrenci bulunamadi!\n\n",num);
+    printf("%d numarali ogrenci bulunamadi!\n\n", num);
 }
 
-//numarasi alinan ogrencinin bilgileri gosteriliyor.
-void ogrBul()
+// numaras� al�nan ��rencinin bilgileri g�steriliyor
+void ogrBul(struct node* head)
 {
-    struct node *iter = head;
+    struct node* iter = head;
     int num;
     printf("Bulmak istediginiz ogrencinin numarasini giriniz: ");
-    scanf("%d",&num);
-    while(iter!=NULL)
+    scanf("%d", &num);
+    while (iter != NULL)
     {
-        if(iter->no==num)
+        if (iter->no == num)
         {
-            printf("\nOgrencinin numarasi: %d\n",iter->no);
-            printf("Ogrencinin adi: %s\n",iter->ad);
-            printf("Ogrencinin soyadi: %s\n",iter->soyad);
-            printf("Ogrencinin bolumu: %s\n",iter->bolum);
-            printf("Ogrencinin sinifi: %d\n\n",iter->sinif);
+            printf("\nOgrencinin numarasi: %d\n", iter->no);
+            printf("Ogrencinin adi: %s\n", iter->ad);
+            printf("Ogrencinin soyadi: %s\n", iter->soyad);
+            printf("Ogrencinin bolumu: %s\n", iter->bolum);
+            printf("Ogrencinin sinifi: %d\n\n", iter->sinif);
             return;
         }
         iter = iter->next;
     }
-    printf("%d numarali ogrenci bulunamadi!\n\n",num);
+    printf("%d numarali ogrenci bulunamadi!\n\n", num);
 }
 
-//listedeki tum ogrenciler sondan basa dogru ekranda gosteriliyor.
-void ogrleriGoster()
+// listedeki t�m ��renciler sondan ba�a do�ru ekranda g�steriliyor
+void ogrleriGoster(struct node* head)
 {
-    struct node *iter = head;
+    struct node* iter = head;
 
-    if(head==NULL)
+    if (head == NULL)
     {
         printf("Listede kayitli ogrenci yok!\n");
     }
     else
     {
-        while(iter!=NULL)
+        while (iter != NULL)
         {
-            printf("\nOgrencinin numarasi: %d\n",iter->no);
-            printf("Ogrencinin adi: %s\n",iter->ad);
-            printf("Ogrencinin soyadi: %s\n",iter->soyad);
-            printf("Ogrencinin bolumu: %s\n",iter->bolum);
-            printf("Ogrencinin sinifi: %d\n\n",iter->sinif);
+            printf("\nOgrencinin numarasi: %d\n", iter->no);
+            printf("Ogrencinin adi: %s\n", iter->ad);
+            printf("Ogrencinin soyadi: %s\n", iter->soyad);
+            printf("Ogrencinin bolumu: %s\n", iter->bolum);
+            printf("Ogrencinin sinifi: %d\n\n", iter->sinif);
             iter = iter->next;
         }
     }
 }
 
-//listedeki her bir dugum free edilerek listeden siliniyor ve listede herhangi bir bilgi kalmiyor.
-void listeyiYokEt()
+// listedeki her bir d���m free edilerek listeden siliniyor ve listede herhangi bir bilgi kalm�yor
+void listeyiYokEt(struct node** head)
 {
-    struct node* iter = head;
+    struct node* iter = *head;
     struct node* diger_dugum;
 
-    while(iter != NULL)
+    while (iter != NULL)
     {
         diger_dugum = iter->next;
         free(iter);
         iter = diger_dugum;
     }
-    head = NULL;
+    *head = NULL;
     printf("Liste siliniyor...\n");
 }
 
 int main()
 {
-    //sonsuz dongu icerisinde kullanicidan komutlar aliniyor.
-    while(1)
+    struct node* head = NULL;
+
+    // sonsuz d�ng� i�erisinde kullan�c�dan komutlar al�n�yor
+    while (1)
     {
         printf("\n--------ANA MENU--------\n\n");
         printf("1.Listeye ogrenci eklemek icin 1'e basiniz.\n");
@@ -165,36 +171,30 @@ int main()
         printf("6.Programi sonlandirmak icin 0'a basiniz.\n");
         printf("Komutu giriniz: ");
         int komut;
-        scanf("%d",&komut);
+        scanf("%d", &komut);
 
-        switch(komut)
+        switch (komut)
         {
-            case 1:
-                //ogrEkle fonksiyonu ile ogrenci bilgileri aliniyor. Alinan bilgiler dugumeOgrEkle fonksiyonuna aktarilip listeye kaydediliyor.
-                ogrEkle();
-                break;
-            case 2:
-                //numarasi girilen ogrenci listeden siliniyor.
-                ogrSil();
-                break;
-            case 3:
-                //numarasi girilen ogrencinin bilgileri listeden bulunup ekrana yazdiriliyor.
-                ogrBul();
-                break;
-            case 4:
-                //guncel listedeki tum ogrencilerin bilgileri sondan basa dogru ekrana yazdiriliyor.
-                ogrleriGoster();
-                break;
-            case 5:
-                //tum listedeki dugumler siliniyor ve liste bos kaliyor.
-                listeyiYokEt();
-                break;
-            case 0:
-                //program kapatiliyor.
-                printf("Program kapatiliyor!");
-                exit(0);
-            default:
-                ;
+        case 1:
+            ogrEkle(&head); // parametre olarak head'i ge�iyoruz
+            break;
+        case 2:
+            ogrSil(&head);
+            break;
+        case 3:
+            ogrBul(head);
+            break;
+        case 4:
+            ogrleriGoster(head);
+            break;
+        case 5:
+            listeyiYokEt(&head);
+            break;
+        case 0:
+            printf("Program kapatiliyor!");
+            exit(0);
+        default:
+            ;
         }
     }
 }
